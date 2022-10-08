ba8eda90f71b2124797ed65b9ff423fc51f445a1
Sydney Wang <sydney.zy.wang@gmail.com>
2022-10-08 14:31:16 -0700

    add anchors

--- inject.js
+++ inject.js
@@ -1,9 +1,16 @@
 (function() {
+	var div = document.createElement('label');
+	div.appendChild(document.createTextNode('You might also want to search for: '));
+	var results = ['fried chicken', 'fries'];
+	results.forEach(suggestion => {
+		var anchorNode = document.createElement('a')
+		anchorNode.appendChild(document.createTextNode(suggestion))
+		anchorNode.href = 'https://www.google.com/search?q='+suggestion
+		div.appendChild(anchorNode);
+		div.appendChild(document.createElement('br'))
 
-	console.log(document);
-	// just place a div at top right
-	var div = document.createElement('div');
-	div.textContent = 'You might also want to search for: fried chicken';
+	}) 
+
 	var temp = document.getElementById('hdtb');
 	temp.prepend(div);
 
