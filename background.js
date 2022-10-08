512fb0996705cc731104b30eced7bbef6c35d1fa
Sydney Wang <sydney.zy.wang@gmail.com>
2022-10-07 22:57:31 -0700

    Add url reader

---
+++ background.js
@@ -0,0 +1,15 @@
+function injectScriptToTab(tabId) {
+  chrome.scripting.executeScript(
+      {
+          target: {tabId: tabId},
+          files: ['inject.js'],
+      }
+  );
+}
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.url) {
+      injectScriptToTab(tabId);
+
+  }
+});
