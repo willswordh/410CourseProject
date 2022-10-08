b5503eff658d4be019efba573d480c781a954e6c
Sydney Wang <sydney.zy.wang@gmail.com>
2022-10-08 13:26:34 -0700

    Add text after google search bar

--- inject.js
+++ inject.js
@@ -1,1 +1,10 @@
-console.log("test test");
+(function() {
+
+	console.log(document);
+	// just place a div at top right
+	var div = document.createElement('div');
+	div.textContent = 'You might also want to search for: fried chicken';
+	var temp = document.getElementById('hdtb');
+	temp.prepend(div);
+
+})();
