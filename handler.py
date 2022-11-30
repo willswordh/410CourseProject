244e715fa5d4593edb9053c8e4b92d5c29fd93d4
Jasper Wang <dw30@illinois.edu>
2022-11-29 21:22:23 -0800

    Completed the implementation for keywords recommendation by constructing document language model and collection language model as well as the comparison process.

--- handler.py
+++ handler.py
@@ -9,45 +9,94 @@ from transformers import pipeline
 import nltk
 from nltk.tokenize import word_tokenize
 
+from collections import Counter
+
+import re
+
+# For POS tagging and lexical analysis
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 
+# Server hosting
 HOST_NAME = 'localhost'
 PORT = 8080
 
+# LM construction
+MAX_NUM_OF_LIKELIHOODS_CONSIDERED = 3
+NUM_OF_RECOMMENDATIONS = 5
 
-
-# def calculateDocLM(inputList) {
-#   aggregatedInput = ' '.join(inputList)
-#   inputWithoutSpecialChar = re.sub('[^A-Za-z\space]','', aggregatedInput)
-#   tokens = inputWithoutSpecialChar.split(' ')
-#   wordDist = Counter(tokens)
-# }
 
 class Processor:
 	def __init__(self):
 		self.model = pipeline('fill-mask', model='bert-base-uncased')
 
+	# Process incoming HTTP POST requests
+	# Input: A list of URLs
+	# Output: Top n words for recommendation for further search. n is defined by NUM_OF_RECOMMENDATIONS constant.
 	def process(self, urls):
-		sentences = self.read_urls(urls)
+		aggregated_input = self.read_urls(urls)
+		doc_LM = self.construct_doc_LM(aggregated_input)
+		print(doc_LM.items())
+		collection_LM = self.construct_collection_LM(aggregated_input)
+		print(collection_LM.items())
+
+		differences = {k: v - collection_LM.get(k, 0) for k, v in doc_LM.items()}
+		sorted_doc_LM_by_scores = sorted(differences.items(), key=lambda x:-x[1])
+		print(sorted_doc_LM_by_scores[:NUM_OF_RECOMMENDATIONS])
+		return list(word for word, prob in sorted_doc_LM_by_scores[:NUM_OF_RECOMMENDATIONS])
+
+	# Construct the collection language model by leveraging BERT fill mask functionality to approximate
+	# Input: A string including all documents
+	# Output: A dictionary of nouns with their corresponding probabilities as the collection language model
+	def construct_collection_LM(self, aggregated_input):
+		sentences = aggregated_input.split('. ')
+		collection_LM = {}
+		total_count = 0
 		for sentence in sentences:
 			# Tokenization
 			words = word_tokenize(sentence)
 
 			# POS tagging for lexical analysis
 			pos_tagged_text = nltk.pos_tag(words)
-
+		
 			# Masking every single noun of the sentence at a time for keyword probability analysis
 			for text, tag in pos_tagged_text:
-				if tag == 'NNS' or tag == 'NNP': # check if tag is NNS or NNP (i.e. noun)
-					input = sentence.replace(text, '[MASK]')
-					output = self.model(input)
-					print('First probability of output:', output[0])
+				if tag == 'NNS': # check if tag is NNS (i.e. noun)
+					# Apple masking exactly once
+					input = sentence.replace(text, '[MASK]', 1)
+					mask_likelihoods = self.model(input)
+					# print(mask_likelihoods)
+					for i in range(min(len(mask_likelihoods), MAX_NUM_OF_LIKELIHOODS_CONSIDERED)):
+						likelihood = mask_likelihoods[i]
 
-		return ['dummy1', 'dummy2']
+						# check if the predicted token contains special characters
+						token_str = likelihood.get('token_str')
+						if re.match('^[a-zA-Z0-9]*$', token_str):
+							collection_LM[token_str] = likelihood.get('score') + collection_LM.get(token_str, 0)
+							total_count = total_count + 1
+		return {k: v / total_count for k, v in collection_LM.items()}
 
+	# Construct Document Language Model using maximum likelihood estimater 
+	# Input: A string including all documents
+	# Output: A dictionary of nouns with their corresponding probabilities as the document language model
+	def construct_doc_LM(self, aggregated_input):
+		# Tokenization
+		words = word_tokenize(aggregated_input)
+
+		# POS tagging for lexical analysis
+		pos_tagged_text = nltk.pos_tag(words)
+
+		filtered = filter(lambda tuple: tuple[1] == 'NNS', pos_tagged_text)
+		all_nouns = [tuple[0] for tuple in list(filtered)]
+		word_dist = Counter(all_nouns)
+		
+		return {k: v / len(all_nouns) for k, v in word_dist.items()}
+
+	# Retrieve all information from a list of links
+	# Input: A list of url links
+	# Output: A string containing all paragraph info from the list of urls
 	def read_urls(self, urls):
-		inputList = []
+		input_list = []
 		# TODO: see if there are ways to batch read urls
 		for url in urls:
 			req = Request(url)
@@ -63,10 +112,11 @@ class Processor:
 				html = response.read()
 				soup = BeautifulSoup(html, 'html.parser')
 				contents = soup('title') + soup('p')
-				inputList = inputList + [data.getText() for data in contents]
-		aggregatedInput = ' '.join(inputList)
-		sentences = aggregatedInput.split('. ')
-		return sentences
+				input_list = input_list + [data.getText() for data in contents]
+		aggregated_input = ' '.join(input_list)
+		return aggregated_input
+
+
 
 class RecommendationRequestHandler(BaseHTTPRequestHandler):
 	protocol_version = 'HTTP/1.1'
